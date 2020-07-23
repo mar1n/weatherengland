3b15b43b0cd662f4aa2cdb79d2c8bce286d6365e
mar1n <30738216+mar1n@users.noreply.github.com>
2020-07-23 22:10:58 +0100

    dynamic generate css on fly, basic on passing prop

--- src/components/atoms/button/button.stories.tsx
+++ src/components/atoms/button/button.stories.tsx
@@ -7,12 +7,16 @@ const buttonClicked = (e: any) => {
 }
 
 export const basicButton = () => <Button>Basic button</Button>;
-
+export const secondaryButton = () => (
+	<Button variant="secondary">Secondary button</Button>
+);
+export const tertiaryButton = () => (
+	<Button variant="tertiary">Tertiary button</Button>
+);
 export const functionButton = () => (
-    <Button onClick={buttonClicked}>Funtion button</Button>
-)
-
-export const linkedButton = () => <Button href="/route"> Linked Button</Button>;
+	<Button onClick={buttonClicked}>Function button</Button>
+);
+export const linkedButton = () => <Button href="/route">Linked button</Button>;
 
 export default {
     title: "Button"
--- src/components/atoms/button/button.tsx
+++ src/components/atoms/button/button.tsx
@@ -1,13 +1,19 @@
 import React, { ReactNode } from "react";
+import StyledButton, { StyledLinkButton } from "./button.styles";
 
 interface ButtonProps {
     children: ReactNode;
     href?: string;
     onClick?: ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void);
+    variant?: string;
 }
-const Button: React.FC<ButtonProps> = ({ children, href, onClick }) => {
-    if(!href) return <button onClick={onClick}>{children}</button>;
-    return <a href={href}>{children}</a>
+const Button: React.FC<ButtonProps> = ({ children, href, onClick, variant }) => {
+    if(!href) return <StyledButton onClick={onClick} variant={variant}>{children}</StyledButton>;
+    return <StyledLinkButton href={href} variant={variant} >{children}</StyledLinkButton>
 };
 
+Button.defaultProps = {
+	children: "Button text",
+	variant: "primary"
+};
 export default Button;
